a4a301ad32c73af71d43b6fb802eea3684af2c99
sorin.popescu1 <sorin.popescu@bunnyshell.com>
2022-05-05 01:37:26 +0300

    many tests

--- complex_fast/variables.tf
+++ complex_fast/variables.tf
@@ -7,6 +7,23 @@ variable "prefix" {
 
 variable "restag" {}
 
+variable "01test" {}
+variable "02test" {}
+variable "03test" {}
+variable "04test" {}
+variable "05test" {}
+variable "06test" {}
+variable "07test" {}
+variable "08test" {}
+variable "09test" {}
+variable "10test" {}
+variable "11test" {}
+variable "12test" {}
+variable "13test" {}
+variable "14test" {}
+variable "15test" {}
+variable "16test" {}
+
 variable "iam_user_name" {
   type = string
   sensitive = true
