fb575963b45493c7cfedc48ac33164f92133a67b
sorin.popescu1 <sorin.popescu@bunnyshell.com>
2022-05-06 17:57:18 +0300

    test p

--- complex_fast/main.tf
+++ complex_fast/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_access_key" "user" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.prefix}-complex-fast-bucket-vineri-test"
+  bucket = "${var.prefix}-complex-fast-bucket-production"
 
   tags = merge(
     var.additional_tags,
