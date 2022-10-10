4fe1396a2a9c30d1487a98304086bf0d675f1c06
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2022-10-10 13:51:28 +0300

    Update main.tf

--- complex_fast/main.tf
+++ complex_fast/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_access_key" "user" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.prefix}-complex-fast-bucket-production-update-test3"
+  bucket = "${var.prefix}-complex-fast-bucket-production-update-test4"
 
   tags = merge(
     var.additional_tags,
