75ad7447d3173669915fb1e2a268397a631a42be
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2022-12-21 13:20:33 +0200

    Update main.tf

--- complex_fast/main.tf
+++ complex_fast/main.tf
@@ -72,6 +72,6 @@ resource "aws_iam_user" "user_alex_test_2" {
   )
 }
 
-resource "aws_iam_access_key" "user_alex_test_1_retest" {
+resource "aws_iam_access_key" "user_alex_test_1_retest_stage" {
   user = aws_iam_user.user_alex_test_2.name
 }
