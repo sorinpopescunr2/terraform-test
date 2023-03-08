9a6e6568b96a54ab10c423e193bab4286cb2bebd
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2023-03-08 19:32:49 +0200

    Update main.tf

--- complex_fast/main.tf
+++ complex_fast/main.tf
@@ -63,7 +63,7 @@ resource "aws_iam_user_policy" "user_policy" {
 }
 
 resource "aws_iam_user" "user_alex_test_2" {
-  name = "${var.prefix}_${var.iam_user_name}_scenario_221027x2_alex_1_retest"
+  name = "${var.prefix}_${var.iam_user_name}_scenario_221027x2_alex_1_retest321"
   tags = merge(
     var.additional_tags,
     {
