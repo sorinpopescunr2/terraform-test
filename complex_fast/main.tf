1038649127f32ec1d8c7b27eaffe5dfc4c410f3c
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2023-05-11 13:41:43 +0300

    Update main.tf

--- complex_fast/main.tf
+++ complex_fast/main.tf
@@ -72,6 +72,6 @@ resource "aws_iam_user" "user_alex_test_2" {
   )
 }
 
-resource "aws_iam_access_key" "user_alex_test_2023_05_11_x1" {
+resource "aws_iam_access_key" "user_alex_test_2023_05_11_x2" {
   user = aws_iam_user.user_alex_test_2.name
 }
