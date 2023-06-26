2465c5096d6c34b23ec56692bc0a12d096e86560
sorinpopescunr2 <96417725+sorinpopescunr2@users.noreply.github.com>
2023-06-26 17:00:32 +0300

    Update outputs.tf

--- lepass/outputs.tf
+++ lepass/outputs.tf
@@ -3,3 +3,10 @@ output "password" {
   value = random_password.password.result
   sensitive = true
 }
+
+
+output "second_password" {
+  description = "The generated password."
+  value = random_password.password.result
+  sensitive = true
+}
